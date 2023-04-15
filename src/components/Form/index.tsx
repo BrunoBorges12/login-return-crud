5451f394c361668eb8a02f6a2981faa5ed092764
BrunoBorges12 <brunoamix084@gmail.com>
2023-04-15 14:25:44 -0300

    estilizando o responsive

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -3,16 +3,16 @@ import Input from "../Input"
 
 function Form (){
         return(
-            <form className="my-20 w-96 font-titilliumWeb">
-                <div className="  flex flex-col  gap-4 mb-10">
-                <h1 className="   text-slate-800	  font-bold text-4xl ">Acesse a plataforma</h1>
-                <h2 className=" text-base text-slate-600">Faça login ou registre-se para começar a construir seus projetos ainda hoje.</h2>
+            <form className="my-20 w-full font-titilliumWeb">
+                <div className="  flex flex-col justify-center  gap-4 mb-10">
+                <h1 className="   text-slate-800	  font-bold lg:text-4xl text-2xl ">Acesse a plataforma</h1>
+                <h2 className=" text-sm  text-slate-600">Faça login ou registre-se para começar a construir seus projetos ainda hoje.</h2>
                 </div>
                     
                 <Input name="email" label="E-email" placeholder="Digite seu e-email" type="email"/>
                 <Input name="password" label="Senha" placeholder="Digite sua senha" type="password" forgotPassword={true}/>
                 <button className=" flex justify-center items-center py-4 px-6 gap-2 rounded bg-violet-800 my-8 w-full text-white  text-base font-titilliumWeb font-bold hover:bg-violet-800/60 transition-all" type="submit">Entrar</button>
-                <div className=" text-base">
+                <div className=" lg:text-base text-sm ">
                     <span className=" text-slate-600">Ainda não tem uma conta?</span>
                     <span className=" text-violet-600 cursor-pointer hover:underline transition-all">Inscreva-se</span>
                 </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,14 +8,14 @@ import Form from '@/components/Form'
 
 export default function Home() {
   return (
-    <main className=' flex w-full h-screen  overflow-hidden font-titilliumWeb ' >
-      <section className='container  px-28   w-1/2' >
-        <div className=' py-10'>
+    <main className=' flex w-full h-screen  lg:overflow-hidden font-titilliumWeb ' >
+      <section className='container   px-8 relative w-full  lg:w-1/2   lg:px-28'  >
+        <div className=' lg:py-10 py-3'>
           <Logo/>
           <Form/>
         </div>
       </section>
-      <section className=' relative  w-1/2'>
+      <section className=' relative lg:block  hidden w-1/2'>
         <div className=' h-screen w-full  bg-violet-600 absolute   mix-blend-screen z-30  '></div>
         <img src="/Rectangle 1.png" alt="Imagem" className=" object-cover inset-0  h-screen w-full       mix-blend-screen  "/>
 
